2e7ef3eceaec7331c79dde5ea3e9152175db8277
fdc-renzo <fdc.renzob@gmail.com>
2024-11-05 14:16:51 +0800

    Initial Commit

--- workspace/cakephp/app/Controller/WelcomeController.php
+++ workspace/cakephp/app/Controller/WelcomeController.php
@@ -3,7 +3,8 @@ class WelcomeController extends AppController
 {
     
     public function index()
-    {
+    {   
+        $this->set('title', 'Message Board');
         $this->set('message', 'Welcome to our application! We are glad to have you here.');
         $this->layout = 'guest'; 
     }
